0ae30ee11a7d420ec56cf18ee1082553972f1da8
Ryan Lertpichitkul <ryl3rt@gmail.com>
2019-11-18 20:35:48 -0800

    UI, HUD, Title Screen

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+
+    int health;
+    GameObject player;
+    Text txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        health = player.GetComponent<Player>().health;
+        txt = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = player.GetComponent<Player>().health;
+        txt.text = "" + health;
+    }
+}
