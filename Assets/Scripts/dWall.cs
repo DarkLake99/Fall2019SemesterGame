28a80056e05728ab9fa9045a8ef0bdbf99a4b90c
Jayds-en <56007875+Jayds-en@users.noreply.github.com>
2019-11-19 21:55:32 -0800

    Did some testing on walls

---
+++ Assets/Scripts/dWall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int health = 100;
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/*
+        if (health == 0){
+            dWall.dissapear();
+        }
+        */
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+      /*      if(other.tag == bullet)
+            {
+                health -= 25;
+            }*/
+    }
+
+    private void dissapear()
+    {
+
+    }
+}
