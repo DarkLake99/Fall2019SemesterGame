c3af2ac9a7d225f618c9ecd16f9f45082d1c8f30
Jayds-en <56007875+Jayds-en@users.noreply.github.com>
2019-11-21 07:40:24 -0800

    Arm rotatio and raycasts

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public int rotationOffset = 90;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition
+            ) - transform.position;
+        difference.Normalize(); // normalise th evector, meaning its sum is 1, (like taking unit vector)
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask notToHit;
+    float timeToFire = 0;
+    Transform firePoint;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        firePoint = transform.Find("firePoint");
+        if(firePoint == null)
+        {
+            Debug.LogError("No firePoint. WHAT");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0)) 
+            Shoot();
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse0)&& Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, notToHit);
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition)*100,Color.white);
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage.");
+        };
+    }
+}
