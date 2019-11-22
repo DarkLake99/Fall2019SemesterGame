46af086a60a28b4c0782f391f6c02d6fd8ad316b
Britney Khuu <britneykhuu@gmail.com>
2019-11-22 08:39:34 -0800

    fonts, fancy ui, another scene, some more edits and yada yadayada

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScreen : MonoBehaviour
+{
+    public GameObject winScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winScreen.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            winScreen.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
