3fc7f094afad81a9d2e8852152398960677fab6b
Britney Khuu <britneykhuu@gmail.com>
2019-11-22 03:47:40 -0800

    added some temp art to work with; fixed the dumb prefab that worked the minute i did not look at it; added gun scripts and items to player but debugging

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    public int rotationOffset = 90;
+    public int rotationOffset = 0;
     // Update is called once per frame
     void Update()
     {
