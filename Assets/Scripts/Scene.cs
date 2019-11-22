43baf1a92df7ce6d009486a68df90e8305049fa4
Britney Khuu <britneykhuu@gmail.com>
2019-11-21 22:31:52 -0800

    changed loadscene script to ints instead of strings for easier scene transitions

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour
 {
-   //took out start and update since it was obsolete to scene class - Britney 11/21/19
-
-    public void LoadLevel(string level)
+   //took out start and update since it was obsolete to scene class - Britney 11/21/1
+   
+   public void LoadLevel(int level)
     {
         SceneManager.LoadScene(level);
     }
